7698d5f0794d8f7ad51d62a657466fe637d4ace0
Matheus Inácio <matheus.bs@ic.ufal.br>
2017-11-07 16:02:02 -0300

    time event 5 min

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
-import utils.ScriptRun;
-
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String args[]) throws IOException {
-        ScriptRun sr = new ScriptRun("$PWD/src/script.py", "");
-        sr.run();
+//        ScriptRun sr = new ScriptRun("$PWD/src/script.py", "");
+//        sr.run();
+        Timer timer = new Timer();
+        TimerTask myTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.printf("hi");
+            }
+        };
+
+        timer.schedule(myTask, 2000, 2000);
     }
+
 }
--- src/laccan/gatewayApp/App.java
+++ src/laccan/gatewayApp/App.java
@@ -1,20 +1,40 @@
 /**
- *
- *  @author <Geymerson Ramos>
- *  @email: <geymerson@laccan.ufal.com>
- *	Universidade Federal de Alagoas - UFAL
- *	Laboratório de Computação Científica e Análise Numérica - LACCAN
- *
- *	last update at August 19, 2017
+ * @author <Geymerson Ramos>
+ * @email: <geymerson@laccan.ufal.com>
+ * Universidade Federal de Alagoas - UFAL
+ * Laboratório de Computação Científica e Análise Numérica - LACCAN
+ * <p>
+ * last update at August 19, 2017
  **/
 
 package laccan.gatewayApp;
 
-import laccan.devices.*;
+import laccan.devices.Micaz;
+import utils.ScriptRun;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class App {
-	public static void main (String[] args) {
-		@SuppressWarnings("unused")
-		Micaz micaz = new Micaz("serial@/dev/ttyUSB1:57600");
-	}
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        Timer timer = new Timer();
+        TimerTask updateDataPredict = new TimerTask() {
+            @Override
+            public void run() {
+                ScriptRun sr = new ScriptRun(
+                        "/home/matheus/IdeaProjects/dtn-cala/script/predict.py",
+                        "/home/matheus/IdeaProjects/dtn-cala/script/data.csv");
+                try {
+                    sr.run();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        timer.schedule(updateDataPredict, 0, 300000);
+        Micaz micaz = new Micaz("serial@/dev/ttyUSB1:57600");
+    }
 }
