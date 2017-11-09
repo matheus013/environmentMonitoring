309e6401636cf58ae184ffa686faebfaa7cbf264
Matheus Inácio <matheus.bs@ic.ufal.br>
2017-11-09 12:02:47 -0300

    implemente load and update data

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-import data.DataGrid;
+import implementations.dm_kernel.user.JCL_FacadeImpl;
+import interfaces.kernel.JCL_facade;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String args[]) throws IOException {
-        DataGrid dataGrid =  new DataGrid();
-
-        dataGrid.collectedToFile();
+        JCL_facade jcl = JCL_FacadeImpl.getInstance();
+        //creating a simple variable
+        System.err.println(jcl.instantiateGlobalVar("firstVar", new String("nada")));
+        jcl.setValueUnlocking("firstVar", "hello world");
     }
 
 }
--- src/data/DataGrid.java
+++ src/data/DataGrid.java
@@ -1,34 +1,47 @@
 package data;
 
+import implementations.dm_kernel.user.JCL_FacadeImpl;
+import interfaces.kernel.JCL_facade;
+import interfaces.kernel.JCL_result;
+import laccan.devices.MicazMsg;
+
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 
 public class DataGrid {
-    protected double[][] predict;
-    protected double[][] collected;
+    JCL_facade jcl;
+    protected HashMap<Point, MicazMsg> predict;
+    protected HashMap<Point, MicazMsg> collected;
 
     public DataGrid() {
-        predict = new double[100][100];
-        collected = new double[100][100];
+        predict = new HashMap<>();
+        collected = new HashMap<>();
+        jcl = JCL_FacadeImpl.getInstance();
     }
 
     public void collectedToFile() throws FileNotFoundException, UnsupportedEncodingException {
         PrintWriter writer = new PrintWriter("collected.csv", "UTF-8");
         int line = 0;
-        for (int i = 1; i <= 100; i++)
-            for (int j = 1; j <= 100; j++) {
-                if (collected[i - 1][j - 1] != 0.0)
-                    writer.println(++line + "," + i + "," + j + "," + collected[i - 1][j - 1]);
-            }
+        for (MicazMsg msg : collected) {
+
+        }
         writer.close();
     }
 
-    public void loadDataCloud(){
+    public void loadDataCloud() {
         //read data from sensor
+        JCL_result result = jcl.getValue("dataCollected");
+        collected = (HashMap<Point, MicazMsg>) result.getCorrectResult();
+
     }
 
-    public void updatePredictCloud(){
+    public void updatePredictCloud() {
         //write prediction from tensorflow
+        if (!jcl.containsGlobalVar("dataPredict"))
+            jcl.instantiateGlobalVar("dataPredict", predict);
+        else jcl.setValueUnlocking("dataPredict", predict);
     }
 }
